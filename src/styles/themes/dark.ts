e33ffb8b99d26343398bf0ab902860aeba67c1c2
unknown <joao.e.c.f@gmail.com>
2020-10-19 21:53:43 +0100

    chore: created a simple theme switcher in react

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,14 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#716A5C',
+    secundary: '#07020D',
+    switch: '#C6E0FF',
+
+    background: '#07020D',
+    text:'#ccc'
+  },
+
+  fonts: {}
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,14 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#2978A0',
+    secundary: '#C6E0FF',
+    switch:'#253031',
+
+    background: '#f5f5f5',
+    text:'#fff'
+  },
+
+  fonts: {}
+}
